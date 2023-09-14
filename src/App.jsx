7091d1d47529a3842b8b93e4dd954540717175c0
Alihaider0315 <ah712200@gmail.com>
2023-09-14 17:13:52 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+import './app.css'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+
+
+const App = () => {
+  return (
+    <div>
+        <Hero/>
+        <Programs/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Programs/programs.css"
+const Programs = () => {
+  return (
+    <div className='programs' id="programs">
+    <div className='programs-headers'>
+    <span className='stroke-text'>Explore Our</span>
+    <span>Programs</span>
+    <span className='stroke-text'>to shape you</span>
+    </div>
+    </div>
+  )
+}
+
+export default Programs
